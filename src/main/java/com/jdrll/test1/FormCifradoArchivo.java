af3a75587c1c57df74c3eb6cc4ae447b2ffdc911
itia_e Julian David Rodríguez Llano <itia_e_jrodriguez@inteia.com.co>
2024-06-25 13:09:04 -0500

    Se  acaba con la funcionalidad del proyecto

--- src/main/java/com/jdrll/test1/FormCifradoArchivo.java
+++ src/main/java/com/jdrll/test1/FormCifradoArchivo.java
@@ -1,14 +1,11 @@
 package com.jdrll.test1;
 
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
+import com.jdrll.test1.ransomewareBackend.Cifrado;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-import java.nio.file.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public class FormCifradoArchivo extends JFrame {
 
@@ -46,18 +43,14 @@ public class FormCifradoArchivo extends JFrame {
                     File selectedFile = fileChooser.getSelectedFile();
                     logArea.append("Archivo seleccionado: " + selectedFile.getAbsolutePath() + "\n");
 
-                    // Generar clave AES automáticamente
                     try {
-                        SecretKey secretKey = generateAESKey();
-                        File keyFile = new File("key.key");
-                        saveKey(secretKey, keyFile);
-
                         // Cifrar el archivo seleccionado
-                        File encryptedFile = new File(selectedFile.getParentFile(), "archivo_cifrado.txt");
+                        File encryptedFile = new File(selectedFile.getParentFile(), getEncryptedFileName(selectedFile));
                         Cifrado.encrypt(selectedFile, encryptedFile);
                         logArea.append("Archivo cifrado exitosamente: " + encryptedFile.getAbsolutePath() + "\n");
 
-                    } catch (Cifrado.CryptoException | IOException ex) {
+                    } catch (Cifrado.CryptoException
+                            ex) {
                         logArea.append("Error durante el cifrado: " + ex.getMessage() + "\n");
                     }
                 }
@@ -73,9 +66,9 @@ public class FormCifradoArchivo extends JFrame {
                     File selectedFile = fileChooser.getSelectedFile();
                     logArea.append("Archivo seleccionado: " + selectedFile.getAbsolutePath() + "\n");
 
-                    // Descifrar el archivo seleccionado
                     try {
-                        File decryptedFile = new File(selectedFile.getParentFile(), "archivo_descifrado.txt");
+                        // Descifrar el archivo seleccionado
+                        File decryptedFile = new File(selectedFile.getParentFile(), getDecryptedFileName(selectedFile));
                         Cifrado.decrypt(selectedFile, decryptedFile);
                         logArea.append("Archivo descifrado exitosamente: " + decryptedFile.getAbsolutePath() + "\n");
 
@@ -90,19 +83,18 @@ public class FormCifradoArchivo extends JFrame {
         setVisible(true);
     }
 
-    private SecretKey generateAESKey() throws Cifrado.CryptoException {
-        try {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            keyGenerator.init(128); // Tamaño de clave AES 128 bits
-            return keyGenerator.generateKey();
-        } catch (NoSuchAlgorithmException ex) {
-            throw new Cifrado.CryptoException("Error generando la clave AES: Algoritmo no encontrado", ex);
-        }
+    private String getEncryptedFileName(File inputFile) {
+        String originalName = inputFile.getName();
+        int dotIndex = originalName.lastIndexOf('.');
+        String nameWithoutExtension = dotIndex == -1 ? originalName : originalName.substring(0, dotIndex);
+        return nameWithoutExtension + ".enc"; // Cambiar la extensión para archivos cifrados
     }
 
-    private void saveKey(SecretKey key, File keyFile) throws IOException {
-        byte[] encodedKey = key.getEncoded();
-        Files.write(keyFile.toPath(), encodedKey);
+    private String getDecryptedFileName(File inputFile) {
+        String originalName = inputFile.getName();
+        int dotIndex = originalName.lastIndexOf('.');
+        String nameWithoutExtension = dotIndex == -1 ? originalName : originalName.substring(0, dotIndex);
+        return nameWithoutExtension + "_decrypted.txt"; // Cambiar la extensión para archivos descifrados
     }
 
     public static void main(String[] args) {
@@ -111,67 +103,5 @@ public class FormCifradoArchivo extends JFrame {
                 new FormCifradoArchivo();
             }
         });
-    }
-}
-
-class Cifrado {
-    private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES";
-
-    public static void encrypt(File inputFile, File outputFile) throws CryptoException {
-        try {
-            SecretKey secretKey = loadKey(new File("key.key"));
-
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-            try (FileInputStream inputStream = new FileInputStream(inputFile);
-                 FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-
-                byte[] inputBytes = new byte[(int) inputFile.length()];
-                inputStream.read(inputBytes);
-
-                byte[] outputBytes = cipher.doFinal(inputBytes);
-
-                outputStream.write(outputBytes);
-            }
-
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException ex) {
-            throw new CryptoException("Error encrypting file", ex);
-        }
-    }
-
-    public static void decrypt(File inputFile, File outputFile) throws CryptoException {
-        try {
-            SecretKey secretKey = loadKey(new File("key.key"));
-
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-            try (FileInputStream inputStream = new FileInputStream(inputFile);
-                 FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-
-                byte[] inputBytes = new byte[(int) inputFile.length()];
-                inputStream.read(inputBytes);
-
-                byte[] outputBytes = cipher.doFinal(inputBytes);
-
-                outputStream.write(outputBytes);
-            }
-
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException ex) {
-            throw new CryptoException("Error decrypting file", ex);
-        }
-    }
-
-    private static SecretKey loadKey(File keyFile) throws IOException {
-        byte[] encodedKey = Files.readAllBytes(Paths.get(keyFile.getAbsolutePath()));
-        return new SecretKeySpec(encodedKey, ALGORITHM);
-    }
-
-    public static class CryptoException extends Exception {
-        public CryptoException(String message, Throwable throwable) {
-            super(message, throwable);
-        }
     }
 }
--- src/main/java/com/jdrll/test1/ransomewareBackend/Cifrado.java
+++ src/main/java/com/jdrll/test1/ransomewareBackend/Cifrado.java
@@ -3,24 +3,26 @@ package com.jdrll.test1.ransomewareBackend;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 public class Cifrado {
     private static final String ALGORITHM = "AES";
     private static final String TRANSFORMATION = "AES";
+    private static SecretKey secretKey;
+
+    // Método para generar la clave si no está definida
+    private static void ensureKeyGenerated() throws CryptoException {
+        if (secretKey == null) {
+            secretKey = generateAESKey();
+        }
+    }
 
     public static void encrypt(File inputFile, File outputFile) throws CryptoException {
+        ensureKeyGenerated();
+
         try {
-            // Generar una clave AES aleatoria
-            SecretKey secretKey = generateAESKey();
-
-            // Guardar la clave generada para usarla en el descifrado
-            saveKey(secretKey, new File("key.key"));
-
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
@@ -35,16 +37,16 @@ public class Cifrado {
                 outputStream.write(outputBytes);
             }
 
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException ex) {
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException |
+                 BadPaddingException | IllegalBlockSizeException | IOException ex) {
             throw new CryptoException("Error encrypting file", ex);
         }
     }
 
     public static void decrypt(File inputFile, File outputFile) throws CryptoException {
-        try {
-            // Cargar la clave AES desde el archivo guardado
-            SecretKey secretKey = loadKey(new File("key.key"));
+        ensureKeyGenerated();
 
+        try {
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
@@ -59,27 +61,20 @@ public class Cifrado {
                 outputStream.write(outputBytes);
             }
 
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException ex) {
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException |
+                 BadPaddingException | IllegalBlockSizeException | IOException ex) {
             throw new CryptoException("Error decrypting file", ex);
         }
     }
 
-    private static SecretKey generateAESKey() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
-        keyGenerator.init(128); // Tamaño de clave AES 128 bits
-        return keyGenerator.generateKey();
-    }
-
-    private static void saveKey(SecretKey key, File keyFile) throws IOException {
-        byte[] encodedKey = key.getEncoded();
-        FileOutputStream fos = new FileOutputStream(keyFile);
-        fos.write(encodedKey);
-        fos.close();
-    }
-
-    private static SecretKey loadKey(File keyFile) throws IOException {
-        byte[] encodedKey = Files.readAllBytes(Paths.get(keyFile.getAbsolutePath()));
-        return new SecretKeySpec(encodedKey, ALGORITHM);
+    private static SecretKey generateAESKey() throws CryptoException {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+            keyGenerator.init(128); // Tamaño de clave AES 128 bits
+            return keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException ex) {
+            throw new CryptoException("Error generando la clave AES: Algoritmo no encontrado", ex);
+        }
     }
 
     public static class CryptoException extends Exception {
